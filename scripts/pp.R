bac0b37ecfa2a57589eca0594bdbd0a155cabbc7
Mark Farrell <mark.farrell@csiro.au>
2021-02-12 18:33:11 +1030

    data in

---
+++ scripts/pp.R
@@ -0,0 +1,19 @@
+#####################################################################################################
+
+#### Thermalox peak-picking script                                                ###################
+
+#### mark.farrell@csiro.au        +61 8 8303 8664         12/02/2021 ################################
+
+#####################################################################################################
+
+#### Set working directory ####
+setwd("/Users/markfarrell/OneDrive - CSIRO/Data/PeakPicking")
+
+
+#### Install packages as needed ####
+
+
+#### Load packages ####
+
+library(tidyverse)
+library(janitor)
