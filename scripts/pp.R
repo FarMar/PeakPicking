9ede1ea7c27c32da69c9c2822d2c5c134c282666
Mark Farrell <mark.farrell@csiro.au>
2021-02-12 18:47:00 +1030

    first workflow OK - peaks picked

--- scripts/pp.R
+++ scripts/pp.R
@@ -11,9 +11,68 @@ setwd("/Users/markfarrell/OneDrive - CSIRO/Data/PeakPicking")
 
 
 #### Install packages as needed ####
+install.packages("zoo")
 
 
 #### Load packages ####
 
 library(tidyverse)
 library(janitor)
+library(zoo)
+
+#### import data ####
+n_raw <- read_csv("data/tn.csv")
+plot(n_raw, col = 'Gray', type = 'l')
+x <- n_raw$time
+y <- n_raw$signal
+
+
+#### smooth ####
+# https://rpubs.com/mengxu/peak_detection?fbclid=IwAR0t8Ni1SvEi86m5yeZL9aQhPE_FxRnR6kFMYVsKX6zQXt3xHVG66g9iPkE
+#local regression
+y.smooth <- loess(y ~ x, span=0.008)$fitted
+plot(x, y.smooth, type = 'l')
+
+#resampling
+w=30
+y.max <- rollapply(zoo(y.smooth), 2*w+1, max, align="center")
+x.max <- rollapply(zoo(x), 2*w+1, median, align="center")
+length(y.max)
+length(x.max)
+plot(x.max, y.max, col = 'Gray', type='l')
+lines(x.max, y.max, col = 'SkyBlue', lwd = 2)
+
+plot(x, y, col = 'Gray', type = 'l')
+lines(x, y.smooth, col = 'Black')
+lines(x.max, y.max, col = 'SkyBlue', lwd = 2)
+
+legend('topleft', c('1', '2', '3'), cex=0.8, col=c('Gray', 'Black', 'SkyBlue'), lty=c(1,1,1))
+
+#peak detection
+n <- length(y)
+delta <- y.max - y.smooth[-c(1:w, n+1-1:w)]
+plot(x.max, delta, type='l')
+abline(h = 0, lty='dotted', col = 'red')
+
+argmax <- function(x, y, w=1, ...) {
+  require(zoo)
+  n <- length(y)
+  y.smooth <- loess(y ~ x, ...)$fitted
+  y.max <- rollapply(zoo(y.smooth), 2*w+1, max, align="center")
+  delta <- y.max - y.smooth[-c(1:w, n+1-1:w)]
+  i.max <- which(delta <= 0) + w
+  list(x=x[i.max], i=i.max, y.hat=y.smooth)
+}
+
+test <- function(w, span) {
+  peaks <- argmax(x, y, w=w, span=span)
+  
+  plot(x, y, cex=0.75, col="Gray", main=paste("w = ", w, ", span = ", span, sep=""))
+  lines(x, peaks$y.hat,  lwd=2) #$
+  y.min <- min(y)
+  sapply(peaks$i, function(i) lines(c(x[i],x[i]), c(y.min, peaks$y.hat[i]), col="Red", lty=2))
+  points(x[peaks$i], peaks$y.hat[peaks$i], col="Red", pch=19, cex=1.25)
+}
+
+test(100, 0.008)
+
